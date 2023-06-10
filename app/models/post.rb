427fdcc6d78e5ca7770119161a3190782415f1c3
veronica365 <naruth10@gmail.com>
2023-06-10 14:12:05 +0300

    Add functionality to increment posts and return latest posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ApplicationRecord
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
   belongs_to :author, class_name: 'User'
+
+  def latest_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def increment_post_counts
+    author.increment!(:post_counter)
+  end
+  after_create :increment_post_counts
 end
