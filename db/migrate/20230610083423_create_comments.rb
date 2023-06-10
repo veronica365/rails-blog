4d9f6243de9b0bbfa5e33d9b88b58aa6fce82d91
veronica365 <naruth10@gmail.com>
2023-06-10 12:20:54 +0300

    generate migration files

---
+++ db/migrate/20230610083423_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :comments do |t|
+      t.belongs_to :author, foreign_key: { to_table: :users }, index: true,  null: false
+      t.belongs_to :post, foreign_key: { to_table: :posts }, index: true,  null: false
+      t.text :text
+      t.timestamps
+    end
+  end
+end
