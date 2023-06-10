5ddf68ce849a6cf3e71c887ce8c10b3637296be3
veronica365 <naruth10@gmail.com>
2023-06-10 14:10:07 +0300

    Add functionality to increment comments and likes counts

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,9 @@
 class Like < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   belongs_to :post, class_name: 'Post', foreign_key: 'post_id'
+
+  def increment_like_counts
+    post.increment!(:likes_counter)
+  end
+  after_create :increment_like_counts
 end
