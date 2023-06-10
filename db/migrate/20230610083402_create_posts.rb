9c05eb2c3ab0364f2d7d4d85c57f4dfd6afafb80
veronica365 <naruth10@gmail.com>
2023-06-10 13:01:54 +0300

    Fix linter errors from codebase

--- db/migrate/20230610083402_create_posts.rb
+++ db/migrate/20230610083402_create_posts.rb
@@ -1,7 +1,7 @@
 class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
-      t.belongs_to :author, foreign_key: { to_table: :users }, index: true,  null: false
+      t.belongs_to :author, foreign_key: { to_table: :users }, index: true, null: false
       t.string :title
       t.text :text
       t.integer :comments_counter
--- db/migrate/20230610083423_create_comments.rb
+++ db/migrate/20230610083423_create_comments.rb
@@ -1,8 +1,8 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.belongs_to :author, foreign_key: { to_table: :users }, index: true,  null: false
-      t.belongs_to :post, foreign_key: { to_table: :posts }, index: true,  null: false
+      t.belongs_to :author, foreign_key: { to_table: :users }, index: true, null: false
+      t.belongs_to :post, foreign_key: { to_table: :posts }, index: true, null: false
       t.text :text
       t.timestamps
     end
