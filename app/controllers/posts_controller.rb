60b8def1cea224f0d29a249b4d6a07c4d633e157
veronica365 <naruth10@gmail.com>
2023-06-12 11:46:16 +0300

    fix linter errors from controller methods

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,9 @@
 class PostsController < ApplicationController
   def index
+    # Post functionality to show all posts by user
   end
 
   def show
+    # Post functionality to show a post by id by user by id
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   def index
+    # User functionality to show all user
   end
 
   def show
+    # User functionality to show a user by id
   end
 end
