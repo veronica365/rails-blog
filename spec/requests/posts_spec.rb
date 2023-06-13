f1993e48c4073dc6c92cf014079041732fba9a15
veronica365 <naruth10@gmail.com>
2023-06-13 14:31:17 +0300

    fix linter errors for the spec files

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-    describe 'GET /index' do
-        it 'Should return the correct response when fetching a user\'s posts' do
-            get '/users/:user_id/posts'
-            expect(response).to have_http_status(:success)
-            expect(response.body).to include('Here is a lists of posts for a given user id')
-        end
+  describe 'GET /index' do
+    it 'Should return the correct response when fetching a user\'s posts' do
+      get '/users/:user_id/posts'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Here is a lists of posts for a given user id')
     end
-    describe 'GET /show' do
-        it 'Should return the correct response when fetching a post by id' do
-            get '/users/:user_id/posts/:id'
-            expect(response).to have_http_status(:success)
-            expect(response.body).to include('Here is a post by id for the given user id')
-        end
+  end
+  describe 'GET /show' do
+    it 'Should return the correct response when fetching a post by id' do
+      get '/users/:user_id/posts/:id'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Here is a post by id for the given user id')
     end
+  end
 end
 
 RSpec.describe PostsController, type: :controller do
-    describe 'GET /users/:user_id/posts' do
-        it 'Should render the correct template' do
-            get :index, params: { user_id: '12' }
-            expect(response).to render_template('posts/index')
-        end
+  describe 'GET /users/:user_id/posts' do
+    it 'Should render the correct template' do
+      get :index, params: { user_id: '12' }
+      expect(response).to render_template('posts/index')
     end
-    describe 'GET /users/:user_id/posts/:id' do
-        it 'Should render the correct template' do
-            get :show, params: { user_id: '12', id: '4'}
-            expect(response).to render_template('posts/show')
-        end
+  end
+  describe 'GET /users/:user_id/posts/:id' do
+    it 'Should render the correct template' do
+      get :show, params: { user_id: '12', id: '4' }
+      expect(response).to render_template('posts/show')
     end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-    describe 'GET /index' do
-        it 'Should return the correct response when fetching users' do
-            get '/users'
-            expect(response).to have_http_status(:success)
-            expect(response.body).to include('Here is a lists of users')
-        end
+  describe 'GET /index' do
+    it 'Should return the correct response when fetching users' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Here is a lists of users')
     end
-    describe 'GET /show' do
-        it 'Should return the correct response when fetching a user by id' do
-            get '/users/:id'
-            expect(response).to have_http_status(:success)
-            expect(response.body).to include('Here is a user by id')
-        end
+  end
+  describe 'GET /show' do
+    it 'Should return the correct response when fetching a user by id' do
+      get '/users/:id'
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include('Here is a user by id')
     end
+  end
 end
 
 RSpec.describe UsersController, type: :controller do
-    describe 'GET /users' do
-        it 'Should render the correct template' do
-            get :index
-            expect(response).to render_template('users/index')
-        end
+  describe 'GET /users' do
+    it 'Should render the correct template' do
+      get :index
+      expect(response).to render_template('users/index')
     end
-    describe 'GET /users/:id' do
-        it 'Should render the correct template' do
-            get :show, params: { id: '4'}
-            expect(response).to render_template('users/show')
-        end
+  end
+  describe 'GET /users/:id' do
+    it 'Should render the correct template' do
+      get :show, params: { id: '4' }
+      expect(response).to render_template('users/show')
     end
+  end
 end
