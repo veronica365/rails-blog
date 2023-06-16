9d75f0690c46ebf3d494fc4081571e8ad422b958
veronica365 <naruth10@gmail.com>
2023-06-16 12:44:17 +0300

    display blog by id by a user id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,8 @@ class PostsController < ApplicationController
     begin
       userid = params[:user_id]
       @posts = Integer(userid) rescue nil
-      return @posts if @posts.nil?   
+      return @posts if @posts.nil?
+
       @user = User.find(@posts)
       @posts = @user.posts
     rescue ActiveRecord::RecordNotFound => e
@@ -12,6 +13,18 @@ class PostsController < ApplicationController
   end
 
   def show
-    # Post functionality to show a post by id by user by id
+    begin
+      userid = Integer(params[:user_id]) rescue nil
+      @post = Integer(params[:id]) rescue nil
+      return @post if @post.nil? || userid.nil?
+
+      @post = Post.find(@post)
+      if @post.author.id != userid
+        @post = nil
+        return
+      end
+    rescue ActiveRecord::RecordNotFound => e
+      @post = nil
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,8 @@ class UsersController < ApplicationController
     begin
       userid = params[:id]
       @user = Integer(userid) rescue nil
-      return @user if @user.nil?      
+      return @user if @user.nil?
+
       @user = User.find(userid)
     rescue ActiveRecord::RecordNotFound => e
       @user = nil
