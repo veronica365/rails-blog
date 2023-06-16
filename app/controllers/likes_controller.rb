b775c28c130a3b733f11c19b7adeed1b43437ef6
veronica365 <naruth10@gmail.com>
2023-06-16 22:50:55 +0300

    fix linter issues with files

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,6 +1,6 @@
 class LikesController < ApplicationController
   before_action :current_post
-    
+
   def new
     @like = Like.new
   end
@@ -23,7 +23,8 @@ class LikesController < ApplicationController
       @postid = nil
     end
 
-    return redirect_to "/users/#{@current_user.id}/posts" unless @postid != nil
+    return redirect_to "/users/#{@current_user.id}/posts" if @postid != nil
+
     @current_post = Post.find(@postid)
-  end  
+  end
 end
