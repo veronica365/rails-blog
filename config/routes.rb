8817ae5a738785ccf5ecf54f45f499cdeb5ee16a
veronica365 <naruth10@gmail.com>
2023-06-20 22:30:32 +0300

    Add functionality to show the pagination button only if there are posts

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :create, :index, :show] do
-      resources :comments, only: [ :new, :create ]
-      resources :likes, only: [ :create ]
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [:create]
     end
   end
 end
