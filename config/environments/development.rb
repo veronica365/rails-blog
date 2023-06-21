c8ca41a9259f2f0fb63d829341c163515444c35a
veronica365 <naruth10@gmail.com>
2023-06-21 13:14:57 +0300

    Reset ruby version and remove bullet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,15 +1,6 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
-  config.after_initialize do
-    Bullet.enable        = true
-    Bullet.alert         = true
-    Bullet.bullet_logger = true
-    Bullet.console       = true
-    Bullet.rails_logger  = true
-    Bullet.add_footer    = true
-  end
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
@@ -67,17 +58,6 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-
-  # Each of these settings defaults to true
-  # Detect N+1 queries
-  Bullet.n_plus_one_query_enable     = true
-
-  # Detect eager-loaded associations which are not used
-  Bullet.unused_eager_loading_enable = true
-
-  # Detect unnecessary COUNT queries which could be avoided
-  # with a counter_cache
-  Bullet.counter_cache_enable        = true
 
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
