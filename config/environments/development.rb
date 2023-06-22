67c4482a7a01f7e82532e4f6de8e2fa903e2eb98
Caren-Koroeny <carensiya5@gmail.com>
2023-06-22 15:06:29 +0300

    Add changes to configure confirmation for an email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,13 +36,24 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
-
-  config.action_mailer.perform_caching = false
-
   # Default URL for the devise mailer
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+  # TODO: Change email and password credentils to match
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                  465,
+    domain:               'gmail.com',
+    user_name:            '--your-gmail-id--@gmail.com',
+    password:             '--your-password--',
+    authentication:       'plain',
+    enable_starttls_auto: true,
+    ssl: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
