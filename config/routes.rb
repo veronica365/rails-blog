8e45fd7206c2bdff06659c51a6ea16d890ab6cd6
veronica365 <naruth10@gmail.com>
2023-06-22 12:22:11 +0300

    Modify functionality to sign up or register a user using devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "login#index"
+  root to: "users#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :create, :index, :show] do
