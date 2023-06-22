30d6d246a15b1cbaeb67b38ec5ef5071e77c3c4d
veronica365 <naruth10@gmail.com>
2023-06-22 04:50:32 +0300

    Run devise generator to install devise files and instructions

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root to: "users#index"
+  root to: "login#index"
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:new, :create, :index, :show] do
